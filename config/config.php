9d1147f9a7e3eb994b2ca138e65f0dab4efcab24
Mick3y16 <pedromiguelfmoreira@gmail.com>
2013-10-22 02:44:48 +0100

    Updated the config file, with new default variables and added Root directory definition aswel...

--- config/config.php
+++ config/config.php
@@ -4,13 +4,14 @@
 define('DEVELOPMENT_ENVIRONMENT', true);
 
 // Database Accesses
-define('DB_HOST','localhost');
-define('DB_AUTH','database1');
-define('DB_CHAR','database2');
-define('DB_USER','username');
-define('DB_PASS','password');
+define ("DB_HOST","localhost");
+define ("DB_AUTH","authdb");
+define ("DB_CHAR","characterdb");
+define ("DB_USER","username");
+define ("DB_PASS","password");
 
 // Directories
+define('DIR_ROOT', $_SERVER['DOCUMENT_ROOT']);
 define('DIR_LOGS','../tmp/logs/error.log');
 
 ?>
